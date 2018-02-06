32be50f485ed543dcf91d5fa111f73fe5550b449
mina1 <mina1@DESKTOP-CQNLRJ0>
2018-02-07 01:15:47 +0900

    8장 클래스 패스와 패키지 _업로드.. 너무 졸려

---
+++ dont-study-java/src/main/java/classPathAndPackage_08/ClassPath.java
@@ -0,0 +1,24 @@
+package classPathAndPackage_08;
+
+class AAA { 
+	public void printName() {
+		System.out.println("AAA");
+	}
+}
+
+class BBB {
+	public void printName() {
+		System.out.println("BBB");
+	}
+}
+
+class ClassPath {
+	public static void main(String[] args) {
+		AAA aaa = new AAA();
+		aaa.printName();
+		
+		BBB bbb =new BBB();
+		bbb.printName();
+	}
+
+}
