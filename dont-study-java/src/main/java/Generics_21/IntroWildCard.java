cbfc6755c94c71c84bf0184b9831a480209b7984
mina455 <mina11027@hanmail.net>
2018-02-20 19:10:22 +0900

    21장 제네릭 _업로드

--- dont-study-java/src/main/java/Generics_21/IntroWildCard.java
+++ dont-study-java/src/main/java/Generics_21/IntroWildCard.java
@@ -25,6 +25,13 @@ class FruitBox3<T> {
 
 class IntroWildCard {
 
+	/**
+	 * 메소드의 매개변수가 FruitBox3<? extends Fruit> 로 선언되었다.
+	 * 이는 Fruit 또는 Fruit을 상속하는 클래스의 이름이 전달되어 생성되는 FruitBox3<T>의
+	 * 인스턴스는 무엇이든 이 메소드의 인자로 전달될 수 있다.
+	 *
+	 * 반대로 extends 대신 super가 들어가면 super 뒤에 오는 클래스가 상속하는 클래스라면 무엇이든...
+	 */
 	public static void openAndShowFruitBox3(FruitBox3<? extends Fruit> box) {
 		Fruit fruit = box.pullOut();
 		fruit.showYou();
